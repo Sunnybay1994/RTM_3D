f4cf1cb78f1487338b2ba536def3e3018ec03ec7
Sunnybay1994 <mbw@mbw_pc>
2020-04-02 17:35:36 +0800

    accomodate to wsl

--- subgeop.py
+++ subgeop.py
@@ -15,34 +15,11 @@ def subg(dirname,nsrc,job_cap=8,proc_num=8):
     fname_sub = os.path.join(workpath,'log','sub.sh')
     fname_rtm0 = os.path.join(workpath,'sub_0offset.sh')
 
-    with open(fname_sub, 'w') as fp:
-            fp.write('''#!/bin/sh
-var1="sub_"; var3=".sh"; for((i=0;i<''' + str(job_cap) + ''';i++)); do var2=`echo $i | awk '{printf("%04d\\n",$0)}'`; qsub ${var1}${var2}${var3}; done''')
-
     if is_zRTM:
         with open(fname_rtm0, 'w') as fp:
             fp.write('''#!/bin/sh
 
-# Lines begin with "#$" are parameters of qsub.
-# Lines begin with "#" except "#!" and "#$" are comments.
-
-# Useage: qsub openmpi.sh
-# Output: <JOB_NAME>.o<JOB_ID>
-
-#$ -cwd
-#$ -m beas
-#$ -j y
-#$ -S /bin/sh
-#$ -v PATH,LD_LIBRARY_PATH
-
-######### set the name of this job
-#$ -N rtm_0offset
-
-######### set Parallel Environment and CORE numbers
-module unload mpi/mpich-x86_64
-module load mpi/openmpi-x86_64
-#$ -pe openmpi '''+ str(proc_num) +'''
-
+NSLOTS=4
 echo "Got $NSLOTS slots."
 echo "PATH = $PATH"
 echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
@@ -51,6 +28,8 @@ DIRNAME="''' + dirname + '''"
 WORKPATH="''' + workpath + '''"
 WORKPATHSTD="''' + stdpath + '''"
 WORKPATHRTM="''' + rtm0path + '''"
+
+cd $WORKPATH
 echo "Current Directory = $WORKPATH"
 echo 
 
@@ -59,7 +38,7 @@ echo "Computing is started at $(date)."
 
 python $PYPATH/pre_RTM_sub.py -m 0 
 echo "Current Directory = $WORKPATHRTM"
-~/software/openmpi-4.0.3/bin/mpiexec -np $NSLOTS -wdir $WORKPATHRTM $WORKPATHRTM/FDTD_MPI 0
+mpiexec -np $NSLOTS -wdir $WORKPATHRTM $WORKPATHRTM/FDTD_MPI 0
 
 echo "Computing is stopped at $(date)."
 
@@ -74,26 +53,7 @@ exit 0
 
         text_head = '''#!/bin/sh
 
-# Lines begin with "#$" are parameters of qsub.
-# Lines begin with "#" except "#!" and "#$" are comments.
-
-# Useage: qsub openmpi.sh
-# Output: <JOB_NAME>.o<JOB_ID>
-
-#$ -cwd
-#$ -m beas
-#$ -j y
-#$ -S /bin/sh
-#$ -v PATH,LD_LIBRARY_PATH
-
-######### set the name of this job
-#$ -N rtm'''+ str(isrc) +'''
-
-######### set Parallel Environment and CORE numbers
-module unload mpi/mpich-x86_64
-module load mpi/openmpi-x86_64
-#$ -pe openmpi '''+ str(proc_num) +'''
-
+NSLOTS=4
 echo "Got $NSLOTS slots."
 echo "PATH = $PATH"
 echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
@@ -103,6 +63,8 @@ WORKPATH="''' + workpath + '''"
 WORKPATHSTD="''' + stdpath + '''"
 WORKPATHRTM="''' + rtmpath + '''"
 WORKPATHRTM0="''' + rtm0path + '''"
+
+cd $WORKPATH
 echo "Current Directory = $WORKPATH"
 echo 
 
@@ -111,47 +73,36 @@ echo "Computing is started at $(date)."
 '''
         if is_zRTM==1:
             text = '''
-~/software/openmpi-4.0.3/bin/mpiexec -np $NSLOTS -wdir $WORKPATH $WORKPATH/FDTD_MPI '''+ str(isrc) +'''
-echo "Current Directory = $WORKPATHSTD"
-~/software/openmpi-4.0.3/bin/mpiexec -np $NSLOTS -wdir $WORKPATHSTD $WORKPATHSTD/FDTD_MPI '''+ str(isrc) +'''
-echo "Current Directory = $WORKPATH"
-#~/software/openmpi-4.0.3/bin/mpiexec -np 1 -wdir $WORKPATH $PYPATH/clean.py -f '''+ str(isrc) +'''
+mpiexec -np $NSLOTS -wdir $WORKPATH $WORKPATH/FDTD_MPI '''+ str(isrc) +'''
+mpiexec -np $NSLOTS -wdir $WORKPATHSTD $WORKPATHSTD/FDTD_MPI '''+ str(isrc) +'''
+
+nohup sh "log/''' + fname_next + '''" > "log/''' + str(isrc + job_cap) + '''.out" 2>&1 &
+
+python $PYPATH/clean.py -f '''+ str(isrc) +'''
 '''
         else:
             text = '''
-~/software/openmpi-4.0.3/bin/mpiexec -np $NSLOTS -wdir $WORKPATH $WORKPATH/FDTD_MPI '''+ str(isrc) +'''
-echo "Current Directory = $WORKPATHSTD"
-~/software/openmpi-4.0.3/bin/mpiexec -np $NSLOTS -wdir $WORKPATHSTD $WORKPATHSTD/FDTD_MPI '''+ str(isrc) +'''
-echo "Current Directory = $WORKPATH"
-~/software/openmpi-4.0.3/bin/mpiexec -np 1 -wdir $WORKPATH $PYPATH/pre_RTM_sub.py '''+ str(isrc) +'''
-echo "Current Directory = $WORKPATHRTM"
-~/software/openmpi-4.0.3/bin/mpiexec -np $NSLOTS -wdir $WORKPATHRTM $WORKPATHRTM/FDTD_MPI '''+ str(isrc) +'''
-#echo "Current Directory = $WORKPATH"
-#~/software/openmpi-4.0.3/bin/mpiexec -np 1 -wdir $WORKPATH $PYPATH/corr_RTM_wavefield_sub.py '''+ str(isrc) +'''
-#~/software/openmpi-4.0.3/bin/mpiexec -np 1 -wdir $WORKPATH $PYPATH/corr_RTM_slice_sub.py '''+ str(isrc) +'''
-#~/software/openmpi-4.0.3/bin/mpiexec -np 1 -wdir $WORKPATH $PYPATH/clean.py '''+ str(isrc) +'''
+mpiexec -np $NSLOTS -wdir $WORKPATH $WORKPATH/FDTD_MPI '''+ str(isrc) +'''
+mpiexec -np $NSLOTS -wdir $WORKPATHSTD $WORKPATHSTD/FDTD_MPI '''+ str(isrc) +'''
+python $PYPATH/pre_RTM_sub.py '''+ str(isrc) +'''
+mpiexec -np $NSLOTS -wdir $WORKPATHRTM $WORKPATHRTM/FDTD_MPI '''+ str(isrc) +'''
+
+nohup sh "log/''' + fname_next + '''" > "log/''' + str(isrc + job_cap) + '''.out" 2>&1 &
+
+python $PYPATH/corr_RTM_wavefield_sub.py '''+ str(isrc) +'''
+python $PYPATH/corr_RTM_slice_sub.py '''+ str(isrc) +'''
+python $PYPATH/clean.py '''+ str(isrc) +'''
 '''
 
         text_tail = '''
 echo "Computing is stopped at $(date)."
 
-qsub ''' + fname_next + '''
 '''
 
         if is_zRTM==1 or is_zRTM==2:
             if isrc == list_src[-1]:
-                text_tail += '\ncd ..\nsh sub_0offset.sh\n'
+                text_tail += '\ncd $WORKPATH\nsh sub_0offset.sh\n'
 
-        if is_zRTM == 1:
-            text_tail += '''
-cd $PYPATH
-python post_put.py -z -t ''' + dirname + ' ' + str(isrc) +'''
-'''
-        else:
-            text_tail += '''
-cd $PYPATH
-python post_put.py -t ''' + dirname + ' ' + str(isrc) +'''
-'''
         text_tail += '\nexit 0\n'
 
         with open(fname, 'w') as fp:
